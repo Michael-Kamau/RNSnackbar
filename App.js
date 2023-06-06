24b1ec56b9ece6dc29788397678ad8b6769b9008
Michael Kamau <kamau.karitu@gmail.com>
2023-06-06 13:22:16 +0300

    -Design adjustments

--- App.js
+++ App.js
@@ -36,39 +36,43 @@ function App() {
   };
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+     flex: 1
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        style={backgroundStyle} contentContainerStyle={{height:'100%', backgroundColor:'#fafafa',position:'relative'}}>
         <View style={{backgroundColor:'#192436'}}>
-          <Text>Afternoon jo</Text>
-          <Text>Here's the latest</Text>
+          <Text style={{color:'white', fontWeight:'bold'}}>Afternoon jo</Text>
+          <Text style={{color:'white'}}>Here's the latest</Text>
           <Text>KES 42,000</Text>
-          <Text>Total funds</Text>
+          <Text style={{color:'white'}}>Total funds</Text>
         </View>
-        <View>
+        <View style={{padding:10}} >
           <Text>Your Goals</Text>
 
 
-          <View style={{flex:1, flexDirection:'row', justifyContent:'space-between', backgroundColor:'red', padding:10}}>
-            <View>
+          <View style={[styles.goal_container,{flexDirection:'row', justifyContent:'space-between', padding:10}]}>
+            <View  >
               <Text>Goal 1</Text>
               <Text>KES 12,000</Text>
-
             </View>
-            <View>
+            <View style={{flexDirection:'row', justifyContent:'space-between', padding:10}}>
+              <Text>Finish Goal</Text>
+              <Text> | </Text>
               <Text>Icon</Text>
             </View>
           </View>
-          <SnackBar ref={snackBarRef} />
+          {/*<SnackBar ref={snackBarRef} />*/}
 
-          <Button onPress={()=>{DisplaySnackBar()}} title=" Show SnackBar ">
-          </Button>
+
         </View>
+
+        {/*<Button style={{position:'absolute',bottom:0,width:20}} onPress={()=>{DisplaySnackBar()}} title=" Show SnackBar ">*/}
+        {/*</Button>*/}
 
       </ScrollView>
     </SafeAreaView>
@@ -91,6 +95,14 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: "700",
+  },
+
+  goal_container: {
+    shadowColor: '#000',            // Shadow color
+    shadowOffset: { width: 0, height: 2 },  // Shadow offset (width, height)
+    shadowOpacity: 0.3,             // Shadow opacity
+    shadowRadius: 4,                // Shadow radius
+    elevation: 4, // Android-specific elevation for shadow
   },
 });
 
